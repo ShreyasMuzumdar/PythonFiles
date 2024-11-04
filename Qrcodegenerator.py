a50bb1855ed3b01601e9520b0d0842bbbd15f92e
Shreyas Muzumdar <Shreyas2966@gmail.com>
2024-11-04 12:37:21 -0500

    newupload

--- Qrcodegenerator.py
+++ Qrcodegenerator.py
@@ -3,7 +3,7 @@ import segno
 folder_path = "/Users/shreyas/Documents/PythonFiles/QRcodes"
 data = input("Enter the data you would like to turn into a qr code:")
 qrcode = segno.make_qr(data)
-file_path = os.path.join(folder_path, "qrcode.png")
+file_path = os.path.join(folder_path, "website.png")
 qrcode.save(
     file_path,
     scale=10,
